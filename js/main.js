1df9bbb5dd327f0cbcf911fa7298e1e01cb0822e
Tikotik <43094326+Tikotik@users.noreply.github.com>
2018-10-15 20:40:35 +0530

    Barebone

---
+++ js/main.js
@@ -0,0 +1,7 @@
+  $(document).ready(function() {
+              $('.modal').modal();
+              $('.tooltipped').tooltip();
+              $('.sidenav').sidenav();
+              $('.parallax').parallax();
+              // feather.replace();
+              });
